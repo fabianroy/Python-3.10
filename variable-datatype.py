a136f04a10bae985186ce2aae3911a7741cccd9d
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-26 17:11:15 +0600

    Create variable-datatype.py

---
+++ variable-datatype.py
@@ -0,0 +1,15 @@
+# Variables and Data Types
+
+x = 1
+y = 1.5
+z = "Hello World!"
+q = 1j
+v = [1, 2, 3, 4, 5]
+w = x + y
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(q))
+print(type(v))
+print(type(w))
